7e8c906f86d75f7de183616016a8c9d32eac464c
Vipul <vipulnitsri2000@gmail.com>
2021-12-15 11:40:25 +0530

    Introduction.java file is added

---
+++ src/Introduction.java
@@ -0,0 +1,34 @@
+/*
+   -Types of languages
+   Procedural,Functional,object oriented
+
+
+   -Static and Dynamic Languages
+
+    Static
+    -perform type checking at compile time.
+    -Error will show at compile time.
+    -Declare datatypes before you use it.
+    -More control.
+
+    Dynamic
+    -Perform type checking at run time.
+    -Error might not show till program is run.
+    -No need to declare datatypes of variables.
+    -Saves time in writing codes but might give error at run time.
+
+    -Garbage collections-Means the objects which dont have any kind of reference variable pointing towards that objects.
+
+ */
+
+
+
+
+public class Introduction {
+
+    public static void main(String[] args) {
+
+        System.out.println("Introduction to java");
+
+    }
+}
