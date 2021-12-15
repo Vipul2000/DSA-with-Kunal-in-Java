f72c63937d9aa8a593c9589531317619ae003ac7
Vipul <vipulnitsri2000@gmail.com>
2021-12-15 14:47:35 +0530

    How java executes files is added

---
+++ src/executes.java
@@ -0,0 +1,18 @@
+/* questions:- How java code executes?
+
+   Answer:- The source code having .java file which is human readable format compiler converts it into .class file which is in byte code format after that interpreter convert line by line code into machine code which is in readable format for machine.
+
+ */
+
+
+
+
+
+
+public class executes {
+    public static void main(String[] args) {
+
+        System.out.println("hurry i execute java code");
+
+    }
+}
