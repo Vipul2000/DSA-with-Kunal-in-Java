8c6a769b90f5cf52078636ea2907a27bb6fef1aa
Vipul <vipulnitsri2000@gmail.com>
2021-12-15 12:01:03 +0530

    flowchart and pseudocode file is added

---
+++ src/flowchartsAndPseudo.java
@@ -0,0 +1,12 @@
+/*Flowcharts and pseudocode
+
+start/stop- ellipse shape is used
+Input/output-Parallelogram shape is used
+Processing- Rectangle shape is used
+Condition- Rhombus shape is used
+*/
+
+
+
+
+
