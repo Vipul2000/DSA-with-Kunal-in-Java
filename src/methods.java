1950136a9e529b67119229a6508084e74562e0e2
Vipul <vipulnitsri2000@gmail.com>
2021-12-16 21:26:30 +0530

    functions and method file is added

---
+++ src/methods.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class methods {
+
+    public static void main(String[] args) {
+
+        //Q:- Take input of 2 numbers and print sum.
+
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First number: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1+num2;
+
+        System.out.println("The sum of two n numbers is: " + sum);*/
+
+
+        //sum();
+        /*int ans = sum2();
+        System.out.println(ans);*/
+
+    }
+
+    /*static void sum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value1: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter the value of num2: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1+num2;
+
+        System.out.println("The sum of two numbers is: " + sum);*/
+
+    /*static int sum2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value1: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter the value of num2: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1+num2;
+
+        return sum;
+
+        //Whenever functions returns somethings it means now the functions is over.Nothing will happen after that.*/
+
+
+
+
+
+
+}
