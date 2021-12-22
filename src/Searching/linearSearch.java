c0740e7a1889056f562d362cbc0af2ed8ea4e85b
Vipul <vipulnitsri2000@gmail.com>
2021-12-22 20:20:05 +0530

    File modified

--- src/Searching/linearSearch.java
+++ src/Searching/linearSearch.java
@@ -1,4 +1,39 @@
 package Searching;
 
+import java.util.Scanner;
+
 public class linearSearch {
+
+    public static void main(String[] args) {
+
+        int[] nums = {23,14,15,25,63,85,22,96};
+        int target = 14;
+
+        int ans = linearSearch(nums , target);
+
+        System.out.println(ans);
+
+    }
+
+    //So basically in linear search we return the index if item found
+    //or return -1 if element not found.
+    //best case time complexity is O(1) i.e constant
+    //worst case time complexity is O(n)
+
+    static int linearSearch(int[] arr, int target ){
+
+        if (arr.length==0){
+            return -1;
+        }
+
+        for (int index = 0; index < arr.length; index++) {
+
+            int element = arr[index];
+            if (element==target){
+                return index;
+            }
+        }
+        return -1;
+
+    }
 }
--- src/arraysAndArraying.java
+++ src/arraysAndArraying.java
@@ -1,7 +1,78 @@
-public class arraysAndArraylist {
+import java.util.Scanner;
+
+public class arraysAndArraying {
     public static void main(String[] args) {
 
 
-        System.out.println("hello world");
+       /*// Q:- store a roll number
+
+       int roll = 26;
+
+       //store a name?
+
+        String name = "Vipul";
+
+        //Store 5 roll no?
+
+        int rno1 = 1;
+        int rno2 = 2;
+        int rno3 = 3;
+
+
+        //synatx of array in java
+
+        //datatype[] variable name = new datatype[size];
+        //store 5 roll number
+
+        int[] rno = new int[5];
+
+        // or directly
+
+        int[] rnos2 = {23,12,15};*/
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        int[] arr = new int[5];
+//        arr[0] = 1;
+//        arr[1] = 12;
+//        arr[2] = 13;
+//        arr[3] = 15;
+//
+//        System.out.println(arr[3]);
+//
+//        //input by using for loop
+//
+//        for (int i = 0; i < arr.length ; i++) {
+//
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        for (int i = 0; i < arr.length ; i++) {
+//            System.out.println(arr[i] + " ");
+
+
+      //  }
+
+        // two diamensional matrices.
+
+        /*
+            1 2 3
+            4 5 6
+            7 8 9
+
+         */
+
+//        int[][] arr = new int[3][];
+//
+//        int arr[][] = {
+//                {1,2,3},
+//                {4,5},
+//                {6,7,8,9}
+//        };
+
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][4];
+        System.out.println(arr.length);
+
     }
 }
