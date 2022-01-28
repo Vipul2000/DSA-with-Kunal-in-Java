8db5423dd9507ddd8132e12931bb7194a21750aa
Vipul <vipulnitsri2000@gmail.com>
2022-01-28 22:55:20 +0530

    Recursion file is added

---
+++ src/Strings/str.java
@@ -0,0 +1,20 @@
+package Strings;
+
+
+//comparison
+public class str {
+    public static void main(String[] args) {
+
+       String a= "kunal";
+       String b="kunal";
+
+        System.out.println(a==b);
+
+
+        String name1= new String("kunal");
+        String name2= new String("kunal");
+
+       // System.out.println(name1==name2);
+        System.out.println(name1.equals(name2));
+    }
+}
